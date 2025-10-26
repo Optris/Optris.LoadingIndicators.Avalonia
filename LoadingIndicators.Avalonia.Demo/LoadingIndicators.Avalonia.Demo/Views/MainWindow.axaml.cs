f3e846cd97770b9ffb579d6c00c6d42c92bd863b
Der_Floh <derr.floh@gmail.com>
2025-10-26 17:36:50 +0100

    fixes and improvements

--- LoadingIndicators.Avalonia.Demo/LoadingIndicators.Avalonia.Demo/Views/MainWindow.axaml.cs
+++ LoadingIndicators.Avalonia.Demo/LoadingIndicators.Avalonia.Demo/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace LoadingIndicators.Avalonia.Demo.Views;
 
@@ -7,5 +8,16 @@ public sealed partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        KeyDown += MainWindow_KeyDown;
+    }
+
+    private void MainWindow_KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Escape)
+            Close();
+
+        if (e.Key == Key.F11)
+            WindowState = WindowState == WindowState.FullScreen ? WindowState.Normal : WindowState.FullScreen;
     }
 }
