4619869dd77d17de4a9d97762e4d4bd03f9ecea6
valentino-saitz-optris <valentino.saitz@optris.de>
2026-04-08 13:36:42 +0000

    Deploying to gh-pages from @ Optris/Optris.LoadingIndicators.Avalonia@f07b4c1ebdf34532ed22b54cb12cad1023837813 🚀

---
+++ _framework/dotnet.boot.js
@@ -0,0 +1,249 @@
+export const config = /*json-start*/{
+  "mainAssemblyName": "LoadingIndicators.Avalonia.Demo.Browser.dll",
+  "resources": {
+    "hash": "sha256-Tl6u0CRpuFCKPXSoE8DX8Yd4mCG0zEoDykan+TTN7MM=",
+    "jsModuleNative": [
+      {
+        "name": "dotnet.native.js"
+      }
+    ],
+    "jsModuleRuntime": [
+      {
+        "name": "dotnet.runtime.js"
+      }
+    ],
+    "wasmNative": [
+      {
+        "name": "dotnet.native.wasm",
+        "integrity": "sha256-A8XXjn9x9m7kdBJP2uisI2aJhdKHhmmLUMCUjZanAcs="
+      }
+    ],
+    "wasmSymbols": [
+      {
+        "name": "dotnet.native.js.symbols"
+      }
+    ],
+    "icu": [
+      {
+        "virtualPath": "icudt_CJK.dat",
+        "name": "icudt_CJK.dat",
+        "integrity": "sha256-SZLtQnRc0JkwqHab0VUVP7T3uBPSeYzxzDnpxPpUnHk="
+      },
+      {
+        "virtualPath": "icudt_EFIGS.dat",
+        "name": "icudt_EFIGS.dat",
+        "integrity": "sha256-8fItetYY8kQ0ww6oxwTLiT3oXlBwHKumbeP2pRF4yTc="
+      },
+      {
+        "virtualPath": "icudt_no_CJK.dat",
+        "name": "icudt_no_CJK.dat",
+        "integrity": "sha256-L7sV7NEYP37/Qr2FPCePo5cJqRgTXRwGHuwF5Q+0Nfs="
+      }
+    ],
+    "coreAssembly": [
+      {
+        "virtualPath": "System.Private.CoreLib.wasm",
+        "name": "System.Private.CoreLib.wasm",
+        "integrity": "sha256-zcoJpbTeL+K6rJysmGFStyG21DBAbszhSbiTSTyDg2s="
+      },
+      {
+        "virtualPath": "System.Runtime.InteropServices.JavaScript.wasm",
+        "name": "System.Runtime.InteropServices.JavaScript.wasm",
+        "integrity": "sha256-UiGdni2ouxsIp057gWlPX102x7iD/cd/srWTWovmLrY="
+      }
+    ],
+    "assembly": [
+      {
+        "virtualPath": "Avalonia.Base.wasm",
+        "name": "Avalonia.Base.wasm",
+        "integrity": "sha256-l/whqAVaIGJ7wSTN/c1+0J/9PxA3bpdytaRXUO9b27g="
+      },
+      {
+        "virtualPath": "Avalonia.Browser.wasm",
+        "name": "Avalonia.Browser.wasm",
+        "integrity": "sha256-+quG562C5KJNxGnWVCdC9SKlNxpA0RReNs6MbmiH530="
+      },
+      {
+        "virtualPath": "Avalonia.Controls.wasm",
+        "name": "Avalonia.Controls.wasm",
+        "integrity": "sha256-D9iXAcEwLWs1va8pPLlwpDUXdXvstRJzy0RzpcuJqj4="
+      },
+      {
+        "virtualPath": "Avalonia.Dialogs.wasm",
+        "name": "Avalonia.Dialogs.wasm",
+        "integrity": "sha256-HNBWSEHg837Dr5Fmchwed59bgT/miQzYLJTlFJMtfDg="
+      },
+      {
+        "virtualPath": "Avalonia.Fonts.Inter.wasm",
+        "name": "Avalonia.Fonts.Inter.wasm",
+        "integrity": "sha256-jSKhtD+fp3mnu8zipe0f57fjkl15DKL+0fup2nEk3OQ="
+      },
+      {
+        "virtualPath": "Avalonia.HarfBuzz.wasm",
+        "name": "Avalonia.HarfBuzz.wasm",
+        "integrity": "sha256-H7oPPYYSKLzY+wwlXdj0b5S7Pc8jKMCkHTxdzzwmIdQ="
+      },
+      {
+        "virtualPath": "Avalonia.Markup.wasm",
+        "name": "Avalonia.Markup.wasm",
+        "integrity": "sha256-PfqN2X5xXKS9LDYLMoeEg5fovKT7KrMH2l87QyP1gxY="
+      },
+      {
+        "virtualPath": "Avalonia.Markup.Xaml.wasm",
+        "name": "Avalonia.Markup.Xaml.wasm",
+        "integrity": "sha256-w9lay4O/eSxhH34BQQE1BEyItwO8OTVVmXv7RxoOHW0="
+      },
+      {
+        "virtualPath": "Avalonia.Metal.wasm",
+        "name": "Avalonia.Metal.wasm",
+        "integrity": "sha256-vaDGVHPbXHZYufG4FVW4ZyBYX43AzcNuKNBHF8SgjCk="
+      },
+      {
+        "virtualPath": "Avalonia.OpenGL.wasm",
+        "name": "Avalonia.OpenGL.wasm",
+        "integrity": "sha256-A48WGB0qeLNTGOTUpKrpJ82QYHYc32W44tJlryNjY3s="
+      },
+      {
+        "virtualPath": "Avalonia.Skia.wasm",
+        "name": "Avalonia.Skia.wasm",
+        "integrity": "sha256-MW9yd9eFmt9MNgFCYr2gcwTGbziE4kKl1kuP/rEtyc4="
+      },
+      {
+        "virtualPath": "Avalonia.Themes.Fluent.wasm",
+        "name": "Avalonia.Themes.Fluent.wasm",
+        "integrity": "sha256-WpGcgGkgJRzOaI60s4U3E0gjGhyrEcrEcZI+Vjp8l8o="
+      },
+      {
+        "virtualPath": "Avalonia.Vulkan.wasm",
+        "name": "Avalonia.Vulkan.wasm",
+        "integrity": "sha256-VULFSKtggD2YUE+MkYxTftieflXXf4aYa/tGOvH3WlQ="
+      },
+      {
+        "virtualPath": "HarfBuzzSharp.wasm",
+        "name": "HarfBuzzSharp.wasm",
+        "integrity": "sha256-uRt0ZnGA36DiQc7JN2GyUFhZ/dtjSvYJ5Qnvlco5Il8="
+      },
+      {
+        "virtualPath": "LoadingIndicators.Avalonia.Demo.Browser.wasm",
+        "name": "LoadingIndicators.Avalonia.Demo.Browser.wasm",
+        "integrity": "sha256-DINnkAslN2Kfl7Tx7Z40kdAZjInXc/4f/Ii50gTFOJ8="
+      },
+      {
+        "virtualPath": "LoadingIndicators.Avalonia.Demo.wasm",
+        "name": "LoadingIndicators.Avalonia.Demo.wasm",
+        "integrity": "sha256-7SN5T2tfB6lWhKImgMk/hlnIt77AljXMOfnBFacQ3Do="
+      },
+      {
+        "virtualPath": "LoadingIndicators.Avalonia.wasm",
+        "name": "LoadingIndicators.Avalonia.wasm",
+        "integrity": "sha256-nJP2wqqDDb1MiDfz9IAizGi5O7VW1//jgAiYIfcw8Mo="
+      },
+      {
+        "virtualPath": "SkiaSharp.wasm",
+        "name": "SkiaSharp.wasm",
+        "integrity": "sha256-LqF3Uu4oeMd1sgvxNUxMbEFKMRgc/XmWmMtSUFRkpx4="
+      },
+      {
+        "virtualPath": "System.Collections.Concurrent.wasm",
+        "name": "System.Collections.Concurrent.wasm",
+        "integrity": "sha256-0j5l6RNe4zNYRo28sGDO3tK0Wc8uT41igl3kUmXo2ZM="
+      },
+      {
+        "virtualPath": "System.Collections.wasm",
+        "name": "System.Collections.wasm",
+        "integrity": "sha256-7EDbM+qrnWPpH0jH6+Qkga5muYOt6fGNFTIaJVAdETY="
+      },
+      {
+        "virtualPath": "System.Collections.NonGeneric.wasm",
+        "name": "System.Collections.NonGeneric.wasm",
+        "integrity": "sha256-xTbFagTS1VEUwmrwfi0vyIsXUq0ElrQouErVjO/X+m8="
+      },
+      {
+        "virtualPath": "System.Collections.Specialized.wasm",
+        "name": "System.Collections.Specialized.wasm",
+        "integrity": "sha256-W8kq9qa0aizgjjdHpt2Rk1BHeg913IQQOib46NQQwrA="
+      },
+      {
+        "virtualPath": "System.ComponentModel.wasm",
+        "name": "System.ComponentModel.wasm",
+        "integrity": "sha256-B37/rmyIYSGtEb0pMRtJnnuO54PeXkEYjuq8waMc3Io="
+      },
+      {
+        "virtualPath": "System.ComponentModel.Primitives.wasm",
+        "name": "System.ComponentModel.Primitives.wasm",
+        "integrity": "sha256-EAYlUMsJkdIwBsKXIYW+fMaaSeGsFTqiJvqQIZIOanQ="
+      },
+      {
+        "virtualPath": "System.ComponentModel.TypeConverter.wasm",
+        "name": "System.ComponentModel.TypeConverter.wasm",
+        "integrity": "sha256-tTeMusAhzGSGZ6rtXA6nDTvNOg73nNAwr+gmJbdjFxw="
+      },
+      {
+        "virtualPath": "System.Diagnostics.DiagnosticSource.wasm",
+        "name": "System.Diagnostics.DiagnosticSource.wasm",
+        "integrity": "sha256-2y1mkW0kGH6YNaYGaRvJf7URCICP0RmCxINuP7EqHzQ="
+      },
+      {
+        "virtualPath": "System.wasm",
+        "name": "System.wasm",
+        "integrity": "sha256-20jVhUzIwd4iBa37tc3g3OaofHfBeJaMcRCVn9IZFoU="
+      },
+      {
+        "virtualPath": "System.Linq.wasm",
+        "name": "System.Linq.wasm",
+        "integrity": "sha256-9qwaW80qZTW68OaSYXdtDiURxolWgt2Em07dz2b/qBs="
+      },
+      {
+        "virtualPath": "System.Linq.Expressions.wasm",
+        "name": "System.Linq.Expressions.wasm",
+        "integrity": "sha256-vvXpb33hVg6TawkPQtH8F1PZjEqzT5jLuxdTWURb604="
+      },
+      {
+        "virtualPath": "System.ObjectModel.wasm",
+        "name": "System.ObjectModel.wasm",
+        "integrity": "sha256-tFtjTOAbnukQzPXXOARK57AJ2kBmo21/FjrTo4a+teU="
+      },
+      {
+        "virtualPath": "System.Private.Uri.wasm",
+        "name": "System.Private.Uri.wasm",
+        "integrity": "sha256-LLnLJf+K8bQLR6LZGcUaL8kce2oVDDHBKupHs3qakh4="
+      }
+    ]
+  },
+  "debugLevel": 0,
+  "globalizationMode": "sharded",
+  "runtimeConfig": {
+    "runtimeOptions": {
+      "configProperties": {
+        "Microsoft.Extensions.DependencyInjection.VerifyOpenGenericServiceTrimmability": true,
+        "System.ComponentModel.DefaultValueAttribute.IsSupported": false,
+        "System.ComponentModel.Design.IDesignerHost.IsSupported": false,
+        "System.ComponentModel.TypeConverter.EnableUnsafeBinaryFormatterInDesigntimeLicenseContextSerialization": false,
+        "System.ComponentModel.TypeDescriptor.IsComObjectDescriptorSupported": false,
+        "System.Data.DataSet.XmlSerializationIsSupported": false,
+        "System.Diagnostics.Debugger.IsSupported": false,
+        "System.Diagnostics.Metrics.Meter.IsSupported": false,
+        "System.Diagnostics.Tracing.EventSource.IsSupported": false,
+        "System.Globalization.Invariant": false,
+        "System.TimeZoneInfo.Invariant": false,
+        "System.Linq.Enumerable.IsSizeOptimized": true,
+        "System.Net.Http.EnableActivityPropagation": false,
+        "System.Net.Http.WasmEnableStreamingResponse": true,
+        "System.Net.SocketsHttpHandler.Http3Support": false,
+        "System.Reflection.Metadata.MetadataUpdater.IsSupported": false,
+        "System.Resources.ResourceManager.AllowCustomResourceTypes": false,
+        "System.Resources.UseSystemResourceKeys": true,
+        "System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeSupported": true,
+        "System.Runtime.InteropServices.BuiltInComInterop.IsSupported": false,
+        "System.Runtime.InteropServices.EnableConsumingManagedCodeFromNativeHosting": false,
+        "System.Runtime.InteropServices.EnableCppCLIHostActivation": false,
+        "System.Runtime.InteropServices.Marshalling.EnableGeneratedComInterfaceComImportInterop": false,
+        "System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization": false,
+        "System.StartupHookProvider.IsSupported": false,
+        "System.Text.Encoding.EnableUnsafeUTF7Encoding": false,
+        "System.Text.Json.JsonSerializer.IsReflectionEnabledByDefault": false,
+        "System.Threading.Thread.EnableAutoreleasePool": false
+      }
+    }
+  }
+}/*json-end*/;
